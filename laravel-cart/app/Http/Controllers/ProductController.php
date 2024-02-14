fae9fe253710ff12597c7dce407d3bd6a3f7fe8c
Vitor Nunes <vitor1nuness@gmail.com>
2024-02-14 11:30:46 -0300

    :sparkles: feat: implements simple CRUD to ProductController

--- laravel-cart/app/Http/Controllers/ProductController.php
+++ laravel-cart/app/Http/Controllers/ProductController.php
@@ -2,63 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(
+        private Product $model,
+    )
+    {
+        //
+    }
+
     public function index()
     {
-        //
+        $products = $this->model->all();
+        return $products;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $product = $request->all();
+        $this->model->create($product);
+        return $product;
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $product = $this->model->findOrFail($id);
+        return $product;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $product = $this->model->findOrFail($id);
+        $data = $request->all();
+        $product->update($data);
+        return $product;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $product = $this->model->findOrFail($id);
+        $product->delete();
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
--- laravel-cart/routes/api.php
+++ laravel-cart/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/products', [ProductController::class, 'index'])->name('index');
+Route::post('/products', [ProductController::class, 'store'])->name('store');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('show');
+Route::patch('/products/{id}', [ProductController::class, 'update'])->name('update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('delete');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
